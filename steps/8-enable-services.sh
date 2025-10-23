421a147cbfe98238b8823ab14eb094af0cacfcab
Stuart Russell <stuart.russell@protonmail.com>
2025-10-23 01:23:54 +0100

    fix mibee

--- steps/8-enable-services.sh
+++ steps/8-enable-services.sh
@@ -7,5 +7,9 @@ systemctl enable fstrim.timer
 pacman -S reflector rsync --noconfirm
 reflector --country 'United Kingdom' --latest 10 --sort rate --fastest 5 --save /etc/pacman.d/mirrorlist
 
-# Install GuiMan
-sudo -u "$1" yay -S guiman --noconfirm
+final_install() {
+    sudo -u "$1" yay -S guiman --noconfirm
+}
+
+# Run final installation steps as the created user
+sudo -u "$1" bash -c "$(declare -f final_install); final_install"
