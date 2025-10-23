1bf33ec561dcb1c783c87e0778c90cbcb1dd7adf
Stuart Russell <stuart.russell@protonmail.com>
2025-10-23 01:07:26 +0100

    username handling again

--- steps/5-enable-yay.sh
+++ steps/5-enable-yay.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Clone yay from AUR and install
-sudo -u $1 git clone https://aur.archlinux.org/yay.git /tmp/yay
-sudo -u $1 cd /tmp/yay
-sudo -u $1 makepkg -si --noconfirm
-sudo -u $1 cd -
-sudo -u $1 rm -rf /tmp/yay
+sudo -u $USERNAME git clone https://aur.archlinux.org/yay.git /tmp/yay
+sudo -u $USERNAME cd /tmp/yay
+sudo -u $USERNAME makepkg -si --noconfirm
+sudo -u $USERNAME cd -
+sudo -u $USERNAME rm -rf /tmp/yay
