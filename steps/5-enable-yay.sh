71f2be595f4ccda3082efc14b6b8e3cf29b32c0b
Stuart Russell <stuart.russell@protonmail.com>
2025-10-23 01:11:08 +0100

    update yay install

--- steps/5-enable-yay.sh
+++ steps/5-enable-yay.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+local user="$1"
+
 # Clone yay from AUR and install
-sudo -u $1 git clone https://aur.archlinux.org/yay.git /tmp/yay
-sudo -u $1 cd /tmp/yay
-sudo -u $1 makepkg -si --noconfirm
-sudo -u $1 cd -
-sudo -u $1 rm -rf /tmp/yay
+install_yay() {
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    cd -
+    rm -rf /tmp/yay
+}
+
+sudo -u "$user" bash -c "$(declare -f install_yay); install_yay"
