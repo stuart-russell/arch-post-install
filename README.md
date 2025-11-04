4ad0a643332ec4e97a105512f838b759cc950de3
Stuart Russell <stuart.russell@protonmail.com>
2025-11-04 15:06:30 +0000

    README

---
+++ README.md
@@ -0,0 +1,14 @@
+# system-setup
+
+This repository contains scripts to automate my personal setup of arch linux. It is intended to streamline the initial system configuration on fresh installs up to the display manager. I have a separate script for installing applications and mapping dotfiles.
+
+## Getting Started
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/stuart-russell/system-setup.git
+    ```
+2. Run the script **not as sudo**:
+    ```sh
+    sh run.sh
+    ```
--- configure-os.sh
+++ configure-os.sh
@@ -2,7 +2,7 @@
 
 install_system_packages() {
     # Update Mirrorlist and Install base packages
-    sudo pacman -S flatpak reflector rsync sudo base-devel git curl wget zip unzip nano vim man net-tools dnsutils noto-fonts noto-fonts-extra ttf-bitstream-vera ttf-dejavu ttf-droid ttf-fira-mono ttf-liberation ttf-opensans ttf-roboto --noconfirm
+    sudo pacman -S flatpak reflector rsync sudo base-devel git curl wget zip unzip nano vim sddm man net-tools dnsutils noto-fonts noto-fonts-extra ttf-bitstream-vera ttf-dejavu ttf-droid ttf-fira-mono ttf-liberation ttf-opensans ttf-roboto --noconfirm
     # Update system packages
     sudo pacman -Syu --noconfirm
 }
