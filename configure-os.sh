eb134a9bd5f2254b44f2880018075f367d07abf0
Stuart Russell <stuart.russell@protonmail.com>
2025-11-05 15:39:00 +0000

    restructure

--- configure-os.sh
+++ configure-os.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source ./sudo-funcs/package-install.sh
-source ./sudo-funcs/service-enable.sh
-
-sudo install_system_packages
+# Update Mirrorlist and Install base packages
+sudo pacman -S flatpak reflector rsync sudo base-devel git curl wget zip unzip nano vim sddm man net-tools dnsutils noto-fonts noto-fonts-extra ttf-bitstream-vera ttf-dejavu ttf-droid ttf-fira-mono ttf-liberation ttf-opensans ttf-roboto --noconfirm
+# Update system packages
+sudo pacman -Syu --noconfirm
 
 reflector --country 'United Kingdom' --latest 10 --sort rate --fastest 5 --save /etc/pacman.d/mirrorlist
 
@@ -21,13 +21,16 @@ rm -rf /tmp/yay
 
 # Install relevant GPU drivers
 if lspci -vnn | grep VGA -A 3 | grep -i nvidia; then
-    pacman -S nvidia nvidia-utils nvidia-settings lib32-nvidia-utils vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
+    sudo pacman -S nvidia nvidia-utils nvidia-settings lib32-nvidia-utils vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
 elif lspci -vnn | grep VGA -A 3 | grep -i amd; then
-    pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon --noconfirm
+    sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon --noconfirm
 fi
 
 # Configure Flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Enable and start services
-sudo enable_services
+sudo systemctl enable fstrim.timer
+sudo systemctl start fstrim.timer
+sudo systemctl enable sddm.service
+sudo systemctl start sddm.service
--- sudo-funcs/service-enable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-enable_services () {
-    systemctl enable fstrim.timer
-    systemctl start fstrim.timer
-    systemctl enable sddm.service
-    systemctl start sddm.service
-}
